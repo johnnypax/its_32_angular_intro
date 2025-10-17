ae9972fca4e0e0c626c4d292a97b3bf531aed013
Giovanni Pace <g.pace@braintechinnovations.it>
2025-10-17 11:53:55 +0200

    Aggiunte strutture

--- 06_servizi/src/app/components/inserisci/inserisci.ts
+++ 06_servizi/src/app/components/inserisci/inserisci.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Studente } from '../../models/studente';
 
 @Component({
   selector: 'app-inserisci',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './inserisci.html',
   styleUrl: './inserisci.css'
 })
 export class Inserisci {
+
+  nom?: string
+  cog?: string
+  mat?: number
+
+  salva(): void{
+    let stu: Studente = {
+      nome: this.nom,
+      cognome: this.cog,
+      matricola: this.mat
+    }
+
+    console.log(stu);
+  }
 
 }
